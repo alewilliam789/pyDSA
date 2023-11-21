75e6061e2b65966351d161f7e0af4bfd957b15f8
Alex <alewilliam789@gmail.com>
2023-11-21 20:11:03 +0200

    Cleanup insertionsort function

--- algorithms/insertionsort/insertionsort.py
+++ algorithms/insertionsort/insertionsort.py
@@ -1,13 +1,15 @@
 def insertionsort(array):
 
 
-    for i,x in enumerate(array):
+    for i in range(len(array)):
         j = i-1
         while(j >=0):
             current_value = array[j+1]
             if(array[j] > current_value):
                 array[j+1] = array[j]
                 array[j] = current_value
+            else:
+                break
             j -= 1
 
 
