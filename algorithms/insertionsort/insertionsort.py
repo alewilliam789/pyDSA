dd11362e56bc59c6d38d24a4ec79add7d547f437
Alex <alewilliam789@gmail.com>
2023-11-21 18:57:48 +0200

    Create venv & implement insertion sort

---
+++ algorithms/insertionsort/insertionsort.py
@@ -0,0 +1,14 @@
+def insertionsort(array):
+
+
+    for element in range(len(array)-1):
+
+        for i,x in enumerate(array):
+            if(i > 0):
+                if(array[i-1] > x):
+                    array[i] = array[i-1]
+                    array[i-1] = x
+
+
+    return array
+
