42d787ec8e4b64dfd1e1585c3c607cf58de69e54
Alex <alewilliam789@gmail.com>
2023-11-29 04:29:47 +0200

    Cleanup

--- algorithms/bubblesort/bubblesort.py
+++ algorithms/bubblesort/bubblesort.py
@@ -8,7 +8,6 @@ def bubblesort(array):
                 if(array[i-1] > x):
                     array[i] = array[i-1]
                     array[i-1] = x
-            print(array)
 
 
     return array
