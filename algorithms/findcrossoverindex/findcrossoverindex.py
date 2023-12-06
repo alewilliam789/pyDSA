ea2cf7daf7850d4da96184a58ee705e6fcca285c
Alex <alewilliam789@gmail.com>
2023-12-06 16:26:26 +0200

    Implement FCI without array

--- algorithms/findcrossoverindex/findcrossoverindex.py
+++ algorithms/findcrossoverindex/findcrossoverindex.py
@@ -1,32 +1,32 @@
-def findCrossoverIndexHelper(x, y, left, right, indexArray):
+def findCrossoverIndexHelper(x, y, left, right):
     
-    if(left >= right):
-        return;
-    
-    elif(left+1 == right):
+    if(left+1 == right):
 
         if(left > 0):
             if(float(x[left-1]) >= float(y[left-1]) and float(x[left]) < float(y[left])):
-                indexArray.append(left-1)
+                return (left-1)
         
         if((float(x[left]) >= float(y[left])) and float(y[right]) > float(x[right])):
-                indexArray.append(left)
+                return left
     else:
         mid = (right+left)//2;
-        findCrossoverIndexHelper(x,y,left,mid, indexArray);
-        findCrossoverIndexHelper(x,y,mid+1,right, indexArray);
-    
+        leftValue = findCrossoverIndexHelper(x,y,left,mid);
+        rightValue = findCrossoverIndexHelper(x,y,mid+1,right);
+        
+        if(leftValue != None and leftValue < len(x)-1):
+            return leftValue;
+        elif(rightValue != None and rightValue < len(x)-1):
+            return rightValue;
+        else:
+            return len(x)-1
 
 
 def findCrossoverIndex(x, y):
     n = len(x)
-    indexArray = [];
     if(n <= 1):
         return 0
     else:
-        findCrossoverIndexHelper(x, y, 0, n-1, indexArray);
-
-    return indexArray[0];
+        return findCrossoverIndexHelper(x, y, 0, n-1);
 
 
 
