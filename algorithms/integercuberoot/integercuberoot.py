aaf87734a894045d73bc460fbac1133f1d53b3ad
Alex <alewilliam789@gmail.com>
2023-12-06 16:07:33 +0200

    Reimplement the algorithm in O(log(n))

--- algorithms/integercuberoot/integercuberoot.py
+++ algorithms/integercuberoot/integercuberoot.py
@@ -1,37 +1,35 @@
-import math
-
-
 def integerCubeRootHelper(n, left, right):
-    mid = (right+left)//2;
-    value = mid*mid*mid;
-
-    while( mid >= left and mid <= right):
-
-        if(value == n):
-            return mid;
-        elif(value < n):
-            mid = (right+mid+1)//2;
-            value = mid*mid*mid;
+    cube = lambda x: x*x*x; # anonymous function to cube a number
+    
+    assert(n >= 1)
+    assert(left < right)
+    assert(cube(left) < n)
+    assert(cube(right) > n)
+    
+    if(left < right):
+        if(left+1 == right):
+            leftValue = cube(left);
+            rightValue = cube(right);
+            if(leftValue <= n or rightValue <=n):
+                if(rightValue <= n):
+                    return right;
+                else:
+                    return left;
+    
         else:
-            while( mid >= left):
-                if(value <= n):
-                    return mid;
-
-                mid -= 1
-                value = mid*mid*mid
-
-
-
-
-
-
-
-
+            mid = (right+left)//2;
+            value = cube(mid);
+            if(value > n):
+                return integerCubeRootHelper(n,left,mid)
+            elif(value < n):
+                return integerCubeRootHelper(n,mid,right)
+            else:
+                return mid
 
 
 def integerCubeRoot(n):
     if(n == 1):
         return 1;
     else:
-        return integerCubeRootHelper(n, 0, math.ceil(math.sqrt(n)))
+        return integerCubeRootHelper(n, 0, n-1)
 
